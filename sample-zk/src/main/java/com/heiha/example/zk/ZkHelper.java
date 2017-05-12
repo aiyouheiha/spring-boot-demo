6a301ea1a054bfcb6d16d5c50e11d4b352311c1f
heiha <aiyouheiha@163.com>
2017-05-12 17:04:26 +0800

    update sample-zk

---
+++ sample-zk/src/main/java/com/heiha/example/zk/ZkHelper.java
@@ -0,0 +1,14 @@
+package com.heiha.example.zk;
+
+import org.springframework.cloud.zookeeper.ZookeeperAutoConfiguration;
+
+/**
+ * <br>
+ * <b>Project:</b> spring-boot-demo<br>
+ * <b>Date:</b> 2017/5/12 17:02<br>
+ * <b>Author:</b> heiha<br>
+ *
+ * @see ZookeeperAutoConfiguration
+ */
+public class ZkHelper {
+}
