f02765a1e078896c9888387bc6cd9660f4bb5e33
heiha <aiyouheiha@163.com>
2017-05-12 17:25:39 +0800

    Add spring-boot-starter-web to include javax.validation.constraints.NotNull

--- sample-zk/src/main/java/com/heiha/example/zk/ZkHelper.java
+++ sample-zk/src/main/java/com/heiha/example/zk/ZkHelper.java
@@ -1,14 +1,13 @@
 package com.heiha.example.zk;
 
-import org.springframework.cloud.zookeeper.ZookeeperAutoConfiguration;
-
 /**
  * <br>
  * <b>Project:</b> spring-boot-demo<br>
  * <b>Date:</b> 2017/5/12 17:02<br>
  * <b>Author:</b> heiha<br>
  *
- * @see ZookeeperAutoConfiguration
+ * @see org.springframework.cloud.zookeeper.ZookeeperProperties
+ * @see org.springframework.cloud.zookeeper.ZookeeperAutoConfiguration
  */
 public class ZkHelper {
 }
