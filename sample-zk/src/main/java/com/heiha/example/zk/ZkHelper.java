a56a54de90e72f32672313bbb970a0e637ee3b33
heiha <aiyouheiha@163.com>
2017-05-15 03:07:49 +0800

    Update sample-zk

--- sample-zk/src/main/java/com/heiha/example/zk/ZkHelper.java
+++ sample-zk/src/main/java/com/heiha/example/zk/ZkHelper.java
@@ -1,5 +1,11 @@
 package com.heiha.example.zk;
 
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.locks.InterProcessSemaphoreMutex;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -8,19 +14,27 @@ import org.springframework.stereotype.Component;
  * <b>Project:</b> spring-boot-demo<br>
  * <b>Date:</b> 2017/5/12 17:02<br>
  * <b>Author:</b> heiha<br>
- *
- * @see org.springframework.cloud.zookeeper.ZookeeperProperties
- * @see org.springframework.cloud.zookeeper.ZookeeperAutoConfiguration
  */
 @Component
-public class ZkHelper {
-
-
+public class ZkHelper implements InitializingBean {
     @Autowired
     private ZookeeperProperties properties;
+
+    private InterProcessSemaphoreMutex lock;
 
     public void use() {
         System.out.println(properties.getConnectString());
         System.out.println(properties.isEnable());
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        CuratorFramework client = CuratorFrameworkFactory.newClient(properties.getConnectString(), retryPolicy);
+        client.start();
+        if (client.checkExists().forPath("/issue") == null) {
+            client.create().forPath("/issue");
+        }
+        lock = new InterProcessSemaphoreMutex(client, "/issue/task-progress-lock");
     }
 }
