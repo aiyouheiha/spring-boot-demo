e31260006502343d3b20d19311f34a7fc09ea023
heiha <aiyouheiha@163.com>
2017-05-17 18:51:36 +0800

    Update sample-mq

---
+++ sample-mq/src/main/java/com/heiha/example/mq/MQHelper.java
@@ -0,0 +1,14 @@
+package com.heiha.example.mq;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * <br>
+ * <b>Project:</b> spring-boot-demo<br>
+ * <b>Date:</b> 2017/5/17 18:51<br>
+ * <b>Author:</b> heiha<br>
+ */
+@Component
+public class MQHelper {
+
+}
