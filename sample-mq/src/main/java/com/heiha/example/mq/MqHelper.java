b8571359c2e7620215f262c8078eac2465bd8f45
heiha <aiyouheiha@163.com>
2017-05-17 18:43:36 +0800

    Update sample-mq

---
+++ sample-mq/src/main/java/com/heiha/example/mq/MqHelper.java
@@ -0,0 +1,17 @@
+package com.heiha.example.mq;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * <br>
+ * <b>Project:</b> spring-boot-demo<br>
+ * <b>Date:</b> 2017/5/17 18:36<br>
+ * <b>Author:</b> heiha<br>
+ *
+ * @see org.springframework.boot.autoconfigure.jms.activemq.ActiveMQProperties
+ * @see org.springframework.boot.autoconfigure.jms.activemq.ActiveMQAutoConfiguration
+ */
+@Component
+public class MQHelper {
+
+}
