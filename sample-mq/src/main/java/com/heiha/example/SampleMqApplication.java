2672aa5b0195408df3444909ea15096dfc20cb54
heiha <aiyouheiha@163.com>
2017-05-17 18:24:19 +0800

    Add sample-mq (Message Queue)

---
+++ sample-mq/src/main/java/com/heiha/example/SampleMqApplication.java
@@ -0,0 +1,12 @@
+package com.heiha.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleMqApplication.class, args);
+	}
+}
