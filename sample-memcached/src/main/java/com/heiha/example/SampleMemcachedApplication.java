4a92205025d786c50b3e622a660a796e4865b422
heiha <aiyouheiha@163.com>
2017-05-18 11:04:25 +0800

    Add sample-memcached

---
+++ sample-memcached/src/main/java/com/heiha/example/SampleMemcachedApplication.java
@@ -0,0 +1,12 @@
+package com.heiha.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleMemcachedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleMemcachedApplication.class, args);
+	}
+}
---
+++ sample-memcached/src/test/java/com/heiha/example/SampleMemcachedApplicationTests.java
@@ -0,0 +1,16 @@
+package com.heiha.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleMemcachedApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
