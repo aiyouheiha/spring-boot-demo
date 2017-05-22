f09aa8af08c2f813b30ae31c6b1adbd72abe4b37
heiha <aiyouheiha@163.com>
2017-05-22 14:27:11 +0800

    sample-mq building

--- sample-mq/src/main/java/com/heiha/example/mq/MQHelper.java
+++ sample-mq/src/main/java/com/heiha/example/mq/MQHelper.java
@@ -1,5 +1,12 @@
 package com.heiha.example.mq;
 
+import com.sun.jndi.ldap.pool.PooledConnectionFactory;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jms.activemq.ActiveMQProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,5 +20,24 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class MQHelper {
+    @Autowired
+    private ActiveMQProperties properties;
+
+    @Bean("jmsFactory")
+    public PooledConnectionFactory pooledConnectionFactory(@Qualifier("activeMQConnectionFactory") ActiveMQConnectionFactory activeMQConnectionFactory) {
+        return null;
+    }
+
+    @Bean("activeMQConnectionFactory")
+    public ActiveMQConnectionFactory activeMQConnectionFactory() {
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(properties.getBrokerUrl());
+        factory.setMaxThreadPoolSize(100);
+        return factory;
+    }
+
+    @Bean
+    public ActiveMQQueue activeMQQueue() {
+        return new ActiveMQQueue("mq.queue");
+    }
 
 }
