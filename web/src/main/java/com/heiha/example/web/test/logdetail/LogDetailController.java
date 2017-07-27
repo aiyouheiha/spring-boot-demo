35d024bedfada60ddb74a1eeef32eb81a079e9ab
heiha <aiyouheiha@163.com>
2017-07-27 11:06:08 +0800

    add necessary intro about last test target

--- web/src/main/java/com/heiha/example/web/test/logdetail/LogDetailController.java
+++ web/src/main/java/com/heiha/example/web/test/logdetail/LogDetailController.java
@@ -15,6 +15,19 @@ import java.util.List;
 @RestController
 @RequestMapping("/test/20170718/logdetail")
 public class LogDetailController {
+    /**
+     * As {@link LogTest#test2} is Boolean, param test should be
+     * <br> ...&test2=true&... or ...&test2=false&...
+     * <br> or ...&test2=&... or without send param test2
+     * <br> <b>If send ...&test2=null&...
+     * Actually, it was sending a String "null", but a real null, and will get an error</b>
+     *
+     * @param s
+     * @param i
+     * @param logTest
+     * @param list
+     * @return
+     */
     @Args
     @RequestMapping(method = RequestMethod.POST)
     public String log(@RequestParam(required = false) String s, @RequestParam(required = false) Integer i, LogTest logTest, @RequestBody List<String> list) {
